749da5df0f0d9092f18376c7660640bc63e8d263
Vishnu Ravi <vishnu.ravi@gmail.com>
2023-05-22 10:32:09 -0400

    Adds filtering to the step count chart (#3)
    * Add date filter to steps graph
    
    * Fix navbar spacing
    
    * Modularizes Activity code
    
    * Fix start and end date filters
    
    * Update footer
    
    * Update header

---
+++ src/components/activity/Activity.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import { Card } from 'react-bootstrap';
+import StepCountChart from './StepCountChart';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+
+const Activity = ({ observations }) => {
+    const endDate = new Date();
+    const startDate = new Date();
+    startDate.setDate(endDate.getDate() - 14); // set the start date to 14 days ago by default
+
+    const [start, setStart] = useState(startDate);
+    const [end, setEnd] = useState(endDate);
+
+    return (
+        <Card className="p-2 m-2 d-flex flex-column align-items-center justify-content-center shadow">
+            <p className="lead">Steps</p>
+            <div style={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center', marginBottom: '20px' }}>
+                <DatePicker selected={start} onChange={(date) => setStart(date)} />
+                <div style={{ margin: '0 10px' }}>to</div>
+                <DatePicker selected={end} onChange={(date) => setEnd(date)} />
+            </div>
+            <StepCountChart observations={observations.entry} startDate={start} endDate={end} />
+        </Card>
+    )
+};
+
+export default Activity;
