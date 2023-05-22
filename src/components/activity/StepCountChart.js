2fcd80ab037cbeb82448aeb4479c3485a9c5d54d
Vishnu Ravi <vishnu.ravi@gmail.com>
2023-05-21 23:05:25 -0400

    Edit x-axis labels

--- src/components/activity/StepCountChart.js
+++ src/components/activity/StepCountChart.js
@@ -2,10 +2,16 @@ import React from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 const StepCountChart = ({ observations }) => {
-  const data = observations.map(entry => ({
-    date: entry.resource.effectiveDateTime,
-    steps: entry.resource.valueQuantity.value,
-  }));
+  const data = observations.map(entry => {
+    const date = new Date(entry.resource.effectiveDateTime);
+    const formattedDate = `${date.toLocaleString('default', { month: 'long' })} ${date.getDate()}`; // Display month as a word
+    
+    return {
+      date: formattedDate,
+      steps: entry.resource.valueQuantity.value,
+      originalDate: date // Store the original date for sorting
+    };
+  }).sort((a, b) => a.originalDate - b.originalDate); // Sort the data by the original date
 
   return (
     <ResponsiveContainer width={'99%'} height={300}>
