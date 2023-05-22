f8a75704f0a9b3d201136fe4646f37ae50b97a04
Vishnu Ravi <vishnu.ravi@gmail.com>
2023-05-21 22:42:13 -0400

    Make graph responsive and add data

--- src/components/activity/StepCountChart.js
+++ src/components/activity/StepCountChart.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 const StepCountChart = ({ observations }) => {
   const data = observations.map(entry => ({
@@ -8,14 +8,16 @@ const StepCountChart = ({ observations }) => {
   }));
 
   return (
-    <LineChart width={600} height={300} data={data}>
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="date" />
-      <YAxis />
-      <Tooltip />
-      <Legend />
-      <Line type="monotone" dataKey="steps" stroke="#8884d8" activeDot={{ r: 8 }} />
-    </LineChart>
+    <ResponsiveContainer width={'99%'} height={300}>
+      <LineChart data={data}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="date" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line type="monotone" dataKey="steps" stroke="#8884d8" activeDot={{ r: 8 }} />
+      </LineChart>
+    </ResponsiveContainer>
   );
 };
 
